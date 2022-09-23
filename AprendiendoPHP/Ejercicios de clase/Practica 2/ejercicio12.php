35245acf21dd9b0e243721e0575b7aeacfdfabec
Alejandro Rodriguez <104910719+MisterioRojo@users.noreply.github.com>
2022-09-23 18:54:36 +0200

    Modificacion y añadir ejemplos de bucles

--- AprendiendoPHP/Ejercicios de clase/Practica 2/ejercicio12.php
+++ AprendiendoPHP/Ejercicios de clase/Practica 2/ejercicio12.php
@@ -11,6 +11,9 @@
 <body>
     <?php
 
+echo "<h1>Ejercicio 2.1</h1>";
+echo "<div>";
+
     $nota1 = 5;
     $nota2 = 8;
     $nota3 = 9;
@@ -32,6 +35,27 @@
     print("Nota mayor: $aNota");
     print("<br>Nota menor: $mNota");
 
+    echo "<h1>Ejercicio 2.2</h1>";
+    echo "<div>";
+    $nota = rand(1, 10);
+    $mejor = $nota;
+    $peor = $nota;
+
+    for ($i = 1; $i <= 10; $i++) {
+        $nota = rand(1, 10);
+
+        if ($nota > $mejor) {
+            $mejor = $nota;
+        }
+
+        if ($nota < $peor) {
+            $peor = $nota;
+        }
+    }
+
+    printf("Mejor nota: %d <br>", $mejor);
+    printf("Peor nota: %d", $peor);
+    echo "</div>";
 
     ?>
 </body>
