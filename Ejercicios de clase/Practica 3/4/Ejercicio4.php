2ae9079853da43b67b9c8452dc4ada32d6d2ff70
AlejandroRodriguezM <aloxfloyd@gmail.com>
2022-09-30 02:44:14 +0200

    Ejercicio 4 y 5

--- Ejercicios de clase/Practica 3/4/Ejercicio4.php
+++ Ejercicios de clase/Practica 3/4/Ejercicio4.php
@@ -1,14 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
-    <title>Ejercicio 2</title>
+    <title>Ejercicio 4</title>
 </head>
+
 <body>
-    
+    <div class="container">
+        <form role="form">
+            <div class="form-group">
+                <h1>
+                    <p>Ejercicio con Formulario.</p>
+                </h1>
+                <label for="numero">Ingrese un numero:</label>
+                <input type="text" name="numero" class="form-control" placeholder="Escribe un numero">
+            </div>
+            <button type="submit" name="Ingresar">Ingresar</button>
+        </form>
+
+        <?php
+
+        function checkNumber($num)
+        {
+            if (is_numeric($num)) {
+                return $num;
+            } else {
+                echo "ERROR. '$num' no es un numero. Ingresa un numero.";
+            }
+        }
+
+        function checkConfirmar()
+        {
+            if (isset($_REQUEST['Ingresar']) == 'Ingresar') {
+                return true;
+            }
+        }
+
+        function numPares($num)
+        {
+            $numeroPar = "";
+            for ($i = 2; $i < $num; $i++) {
+                if (($i % 2) == 0) {
+                    $numeroPar .= $i . "-";
+                }
+            }
+            echo $numeroPar;
+        }
+
+        function mostrarPares()
+        {
+            if (is_bool(checkConfirmar())) {
+                $num = $_REQUEST['numero'];
+                if (checkNumber($num)) {
+                    numPares($num);
+                }
+            }
+        }
+
+        mostrarPares();
+        ?>
+    </div>
 </body>
+
 </html>
--- Ejercicios de clase/Practica 3/5/Ejercicio5.php
+++ Ejercicios de clase/Practica 3/5/Ejercicio5.php
@@ -1,14 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
-    <title>Ejercicio 2</title>
+    <title>Ejercicio 5</title>
 </head>
+
 <body>
-    
+    <div class="container">
+        <form role="form">
+            <div class="form-group">
+                <h1>
+                    <p>Ejercicio con Formulario</p>
+                </h1>
+                <label for="nombre">Nombre:</label>
+                <input type="text" class="form-control" name="nombre" placeholder="Ingrese el nombre">
+                <br>
+                <label for="apellido">Nombre:</label>
+                <input type="text" class="form-control" name="apellido" placeholder="Ingrese el apellido">
+                <br>
+                <label for="ciudad">Nombre:</label>
+                <input type="text" class="form-control" name="ciudad" placeholder="Ingrese la ciudad">
+            </div>
+            <br>
+            <button type="submit" name="confirmar">Ingresar</button>
+            <br>
+        </form>
+
+        <?php
+
+        function checkText()
+        {
+            $textoCompleto = "";
+            $name = $_REQUEST['nombre'];
+            $lastName = $_REQUEST['apellido'];
+            $city = $_REQUEST['ciudad'];
+
+            if(strlen($name) != 0 and strlen($lastName) != 0 and strlen($city) != 0){
+                echo "$name $lastName vive en $city";
+            }
+        }
+
+        function checkConfirmar(){
+            if(isset($_REQUEST['confirmar']) == 'confirmar'){
+                return true;
+            }
+        }
+
+        function verFrase(){
+            if(is_bool(checkConfirmar())){
+                checkText();
+            }
+        }
+        verFrase();
+
+        ?>
+
+    </div>
+
 </body>
+
 </html>
